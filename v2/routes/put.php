c3547c15260710efd2139b4a95b9dfb65851eb44
Oliver Richman <oliver.richman@outlook.com>
2019-05-02 19:06:49 +0100

    remove unused version

--- v2/routes/put.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (empty($endpoint_id)) {
-    response(403, 'You need to specify an ID to edit.');
-} else {
-    // Edit $endpoint_id on $endpoint
-    echo 'Edit id:' . $endpoint_id . ' on ' . $endpoint;
-}
